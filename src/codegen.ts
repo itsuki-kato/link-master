355d5349ff0c078366ddc591a0e4eba28d8c80b0
TABLET-6FSNT7EB\gojin <larc.ituki@gmail.com>
2024-08-04 12:50:47 +0900

    graphql-codegeneraterとapollo-clientのセットアップ

---
+++ src/codegen.ts
@@ -0,0 +1,20 @@
+
+import type { CodegenConfig } from '@graphql-codegen/cli';
+
+const config: CodegenConfig = {
+  overwrite: true,
+  schema: "http://localhost:3100/graphql",
+  documents: "src/generates",
+  ignoreNoDocuments: true,
+  generates: {
+    "src/gql/": {
+      preset: "client",
+      plugins: []
+    },
+    "./graphql.schema.json": {
+      plugins: ["introspection"]
+    },
+  }
+};
+
+export default config;
--- src/src/app/layout.tsx
+++ src/src/app/layout.tsx
@@ -3,7 +3,6 @@
 import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 import {
-  DesktopOutlined,
   FileOutlined,
   PieChartOutlined,
   TeamOutlined,
